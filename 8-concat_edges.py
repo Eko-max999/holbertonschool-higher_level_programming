33f91aab7c54840abafb460134f5c40edeff43dd
Eko-max999 <13118@holbertonschool.com>
2026-02-09 19:41:59 +0000

    Task 8 completed

---
+++ 8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str1 = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+str2 = "Python is a programming language that lets you work quickly and integrate your systems more effectively"
+str1 = str1[39:67] + str1[106:112] + str1[:6]
+print(str1)
