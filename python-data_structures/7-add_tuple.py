2ba3de84daa0f3899dda5af32fe0e00fdfd0f0e9
Eko-max999 <13118@holbertonstudents.com>
2026-02-14 11:38:16 +0000

    Fix W293 whitespace on task 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Tuple-lara (0, 0) elave edirik ki, index xetasi olmasin
     a = tuple_a + (0, 0)
     b = tuple_b + (0, 0)
-    
-    # Ancaq ilk iki elementi cemleyirik
     return (a[0] + b[0], a[1] + b[1])
